0ef2cadc91d1ccb9fade0e8f50c929136cdc3767
Jacob <jacob.saini60@gmail.com>
2021-03-05 09:03:26 +1000

    add components/services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TabsComponent } from './tabs/tabs.component';
+
+const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'tabs', component: TabsComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
